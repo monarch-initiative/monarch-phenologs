90507c5968f2d495b9a29a61909e2b7a52916eaf
bryanlaraway <bryanlaraway@gmail.com>
2024-08-06 17:18:25 -0700

    Adding in some code for generation of phenotype-ortholog matrix, including updates of the matrix values.

--- transforms/phenologs/duckdb/xx_duck_db_matrix_test.py
+++ transforms/phenologs/duckdb/xx_duck_db_matrix_test.py
@@ -1,7 +1,7 @@
 import duckdb
 import numpy as np
 import pandas as pd
-
+from scipy.stats import hypergeom, pearsonr
 
 
 
@@ -35,7 +35,7 @@ duckdb.sql("SELECT * FROM taxons").show(max_width=10000, max_rows=10)
 # TODO: I'm thinking the latter is more appropriate, but double-check.
 # Also, should we be using row numbers - 1 to align with the matrix coordinates?
 duckdb.sql("CREATE TABLE knn_orthologs AS "
-           "SELECT row_number() OVER (ORDER BY (ortholog_id)) - 1 as i, * FROM ( "
+           "SELECT row_number() OVER (ORDER BY (ortholog_id)) - 1 as j, * FROM ( "
            "SELECT DISTINCT ortholog_id from orthologs "
            "WHERE "
            "gene_a_taxon_id in (select taxon_id from taxons) "
@@ -52,7 +52,7 @@ duckdb.sql("SELECT count(*) FROM knn_orthologs").show(max_width=10000, max_rows=
 # Need to generate a phenotype +10 nearest neighbors table
 # *****Create phenotype IDs as phenotype_taxon?
 duckdb.sql("CREATE TABLE knn_phenotypes AS "
-           "SELECT row_number() OVER (ORDER BY (phenotype_id))- 1 as j, * FROM ( "
+           "SELECT row_number() OVER (ORDER BY (phenotype_id))- 1 as i, * FROM ( "
            "SELECT DISTINCT pto.phenotype_id, pto.phenotype_name, pto.phenotype_taxon_id, concat(pto.phenotype_id, '_', pto.phenotype_taxon_id) as phenotaxon_id, "
            # "row_number() OVER (ORDER BY (SELECT NULL)) as j, "
            "null as knn_phenotype_1, "
@@ -90,13 +90,19 @@ duckdb.sql("SELECT count(*) FROM knn_phenotypes").show(max_width=10000, max_rows
 
 # In preparation for filling out the matrix, generate the phenotype_to_ortholog table with the phenotaxon_id added:
 duckdb.sql("CREATE TABLE phenotype_ortholog_matrix_input AS "
-           "SELECT DISTINCT pto.phenotype_id, pto.phenotype_name, pto.phenotype_taxon_id, concat(pto.phenotype_id, '_', pto.phenotype_taxon_id) as phenotaxon_id, ortholog_id "
-           "FROM phenotype_to_ortholog pto ")
+           "SELECT DISTINCT pto.phenotype_id, pto.phenotype_name, pto.phenotype_taxon_id, "
+           "concat(pto.phenotype_id, '_', pto.phenotype_taxon_id) as phenotaxon_id, pto.ortholog_id, "
+           "p.i, o.j "
+           "FROM phenotype_to_ortholog pto "
+           "LEFT JOIN knn_phenotypes p "
+           "ON concat(pto.phenotype_id, '_', pto.phenotype_taxon_id) = p.phenotaxon_id "
+           "LEFT JOIN knn_orthologs o "
+           "ON pto.ortholog_id = o.ortholog_id")
 
 print('phenotype_ortholog_matrix_input Table: ')
 duckdb.sql("SELECT * FROM phenotype_ortholog_matrix_input order by phenotaxon_id").show(max_width=10000, max_rows=10)
 
-print('kNN Phenotypes Table row count: ')
+print('phenotype_ortholog_matrix_input row count: ')
 duckdb.sql("SELECT count(*) FROM phenotype_ortholog_matrix_input").show(max_width=10000, max_rows=10)
 
 
@@ -110,9 +116,12 @@ ortholog_count = duckdb.sql("SELECT count(*) FROM knn_orthologs").fetchall()
 ortholog_count = ortholog_count[0][0]
 print("Ortholog count:" + str(ortholog_count))
 
-phenolog_count = duckdb.sql("SELECT count(*) FROM knn_phenotypes").fetchall()
-phenolog_count = phenolog_count[0][0]
-print("Phenolog count:" + str(phenolog_count))
+phenotype_count = duckdb.sql("SELECT count(*) FROM knn_phenotypes").fetchall()
+phenotype_count = phenotype_count[0][0]
+print("Phenotype count:" + str(phenotype_count))
+
+matrix_size = phenotype_count * ortholog_count
+print("Phenotype-ortholog matrix size (total cells): " + str(matrix_size))
 
 # pandas test
 # pandas_count = duckdb.df("SELECT count(*) FROM knn_phenotypes").fetchall()
@@ -126,19 +135,60 @@ print("Phenolog count:" + str(phenolog_count))
 # We have the phenotypes and the orthologs available with numeric identifiers/row numbers starting from 0,
 # and these could be used as phenotype-ortholog matrix coordinates.
 
-phenotype_ortholog_matrix = np.zeros((phenolog_count, ortholog_count))
+phenotype_ortholog_matrix = np.zeros((phenotype_count, ortholog_count), dtype=np.int8)
+
+'''
+FOR REFERENCE: Phenotypes: i, Orthologs: j
+Conceptualizing the phenotype ortholog matrix and the downstream calculations:
+Note that the phenotype-ortholog matrix is a matrix of all orthologs that are present for the included taxons, 
+regardless of whether or not an association of that ortholog with a phenotype exists. We' re including the full "ortholog space."
+Then when we compare two phenotypes, we take the representative slices of the matrix for each phenotype
+(or more specifically each "phenotaxon") and compare those slices using Pearson correlation.
+'''
+
+
 
 # Here need to iterate through and add a 1 for every row in knn_phenotypes where there's a corresponding row in knn
 # What's the best approach? Convert the duckdb tables to pandas/polars dataframes, iterate through?
 
+# STEPS:
 # Iterate through the rows in the newly constructed phenotype_ortholog_matrix_input table:
 # Columns: phenotype_id, phenotype_name, phenotype_taxon_id, phenotaxon_id, ortholog_id
+# Self comparison? Where phenotaxon_id <> phenotaxon_id and phenotype_taxon_id <> phenotype_taxon_id?
+
+# Place a 1 at the matrix coordinates corresponding to the ith ortholog, jth phenotype.
+# Best way of extracting these coordinate values? Should add the i/j columns to the phenotype_ortholog_matrix_input
 
 
+# read the result of an arbitrary SQL query to a Pandas DataFrame
+coordinates_df = duckdb.sql("SELECT i, j from phenotype_ortholog_matrix_input").df()
+print(coordinates_df)
+# counter = 0
+for row in coordinates_df.itertuples():
+    # print(row)
+    i = row.i
+    j = row.j
+    # counter += 1
+    # print("i: " + str(i) + ", j: " + str(j))
+    phenotype_ortholog_matrix[i][j] = 1
 
-
+print(phenotype_ortholog_matrix[10333][3368])
 
 np.save('../../../datasets/intermediate/duckdb_tables/phenotype_ortholog_matrix.npy', phenotype_ortholog_matrix)
 np.savetxt('../../../datasets/intermediate/duckdb_tables/phenotype_ortholog_matrix.txt', phenotype_ortholog_matrix)
 
+# Just to test, take two rows of the phenotype_ortholog_matrix and compare:
+row_a = phenotype_ortholog_matrix[10333]
+row_b = phenotype_ortholog_matrix[10332]
+print(row_a)
+print(row_b)
+(coefficient, p_value) = pearsonr(phenotype_ortholog_matrix[10333],
+                                          phenotype_ortholog_matrix[10332])
+print(coefficient)
+print(p_value)
+
+
+# TODO:
+# Would need to iterate through every phenotaxon_id vs phenotaxon_id (aka updated phenologs base table)
+# and grab the pearson coefficient for the corresponding rows of the phenotype-ortholog matrix.
 
